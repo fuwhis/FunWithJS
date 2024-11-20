9d5b80b292cca8de89f62a35b0c7f98391693b11
fuwhis <steve.parker117@gmail.com>
2024-11-20 15:18:51 +0700

    refactor: :sparkles: add functions transform speech to text included `webkitSpeechRecognition()` constructor

--- speech-to-text/script.js
+++ speech-to-text/script.js
@@ -1,3 +1,50 @@
-function startConverting() {}
+const resultElement = document.getElementById('result')
+let recognition
 
-function stopConverting() {}
+function startConverting() {
+  if ('webkitSpeechRecognition' in window) {
+    recognition = new webkitSpeechRecognition()
+
+    setupRecognition(recognition)
+
+    recognition.start()
+  }
+}
+
+function setupRecognition(recognition) {
+  recognition.continuous = true
+
+  recognition.interimResults = true
+
+  recognition.lang = 'en-US'
+
+  recognition.onresult = function (event) {
+    const { finalTranscript, interTranscript } = processResult(event.results)
+
+    // Update transcript into #result element
+    resultElement.innerHTML = finalTranscript + interTranscript
+  }
+}
+
+function processResult(results) {
+  let finalTranscript = ''
+  let interTranscript = ''
+  for (let i = 0; i < results.length; i++) {
+    let transcript = results[i][0].transcript
+    transcript.replace('\n', '<br>')
+
+    if (results[i].isFinal) {
+      finalTranscript += transcript
+    } else {
+      interTranscript += transcript
+    }
+  }
+
+  return { finalTranscript, interTranscript }
+}
+
+function stopConverting() {
+  if (recognition) {
+    recognition.stop()
+  }
+}
