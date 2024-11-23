e0919e2b39bbf4f9bf29e5f1fc7b1fcda2d761a4
fuwhis <steve.parker117@gmail.com>
2024-11-24 01:03:02 +0700

    fix: :bug: fixed bug of *color-display* rendering

--- hsl-color-generator/script.js
+++ hsl-color-generator/script.js
@@ -1,31 +1,53 @@
-// Select element
-const hueSlider = document.getElementById('hue')
-const saturationSlider = document.getElementById('saturation')
-const lightnessSlider = document.getElementById('lightness')
-const hueValue = document.getElementById('hueValue')
-const saturationValue = document.getElementById('saturationValue')
-const lightnessValue = document.getElementById('lightnessValue')
-const hslValueInput = document.getElementById('hslValue')
-const colorDisplay = document.querySelector('.color-display')
+document.addEventListener('DOMContentLoaded', function () {
+  // Select element
+  const hueSlider = document.getElementById('hue')
+  const saturationSlider = document.getElementById('saturation')
+  const lightnessSlider = document.getElementById('lightness')
 
-function updateHSL() {
-  const hue = hueSlider.value
-  const saturation = saturationSlider.value
-  const lightness = lightnessSlider.value
+  const hueValue = document.getElementById('hueValue')
+  const saturationValue = document.getElementById('saturationValue')
+  const lightnessValue = document.getElementById('lightnessValue')
 
-  hueValue.textContent = hue
-  saturationValue.textContent = saturation
-  lightnessValue.textContent = lightness
+  const colorDisplay = document.querySelector('.color-display')
+  const hslValueInput = document.getElementById('hslValue')
 
-  const hsl = `${hue}°, ${saturation}%, ${lightness}%`
+  const copyButton = document.getElementById('copyButton')
 
-  // Bind the value to the input field and update color display
-  hslValueInput.value = hsl
-  colorDisplay.style.backgroundColor = hsl
-}
+  function updateHSL() {
+    const hue = hueSlider.value
+    const saturation = saturationSlider.value
+    const lightness = lightnessSlider.value
 
-hueSlider.addEventListener('input', updateHSL)
-saturationSlider.addEventListener('input', updateHSL)
-lightnessSlider.addEventListener('input', updateHSL)
+    hueValue.textContent = hue
+    saturationValue.textContent = saturation
+    lightnessValue.textContent = lightness
 
-updateHSL()
+    const hsl = `${hue}°, ${saturation}%, ${lightness}%`
+    const color = `hsl(${hue}, ${saturation}%, ${lightness}%)`
+
+    // Bind the value to the input field and update color display
+    hslValueInput.value = hsl
+    colorDisplay.style.backgroundColor = color
+  }
+
+  copyButton.addEventListener('click', copyToClipboard)
+
+  function copyToClipboard() {
+    const textToCopy = `hsl(${hueSlider.value}, ${saturationSlider.value}%, ${lightnessSlider.value}%)`
+
+    navigator.clipboard
+      .writeText(textToCopy)
+      .then(function () {
+        alert('✅ The color is Copy to Clipboard.')
+      })
+      .catch(function (err) {
+        console.log('Unable to Copy the Color', err)
+      })
+  }
+
+  hueSlider.addEventListener('input', updateHSL)
+  saturationSlider.addEventListener('input', updateHSL)
+  lightnessSlider.addEventListener('input', updateHSL)
+
+  updateHSL()
+})
