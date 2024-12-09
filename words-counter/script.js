236d4f60e8cf8b3548951a28ff929f30237371d6
fuwhis <steve.parker117@gmail.com>
2024-12-09 16:24:58 +0700

    perf: :zap: improved performance and splited code

--- words-counter/script.js
+++ words-counter/script.js
@@ -1,23 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   const input = document.querySelector("textarea");
-  const characterCount = document.querySelector("#characterCount");
-  const wordCount = document.querySelector("#wordCount");
-  const sentenceCount = document.querySelector("#sentenceCount");
+  [characterCount, wordCount, sentenceCount] = [
+    document.querySelector("#characterCount"),
+    document.querySelector("#wordCount"),
+    document.querySelector("#sentenceCount"),
+  ];
 
-  input.addEventListener("input", function () {
-    const text = input.value.trim();
+  /**
+   * Calculate Function
+   * @param {typed texture in textare} text
+   */
+  const calculateCounts = (text) => {
+    const trimmedText = input.value.trim();
 
     // Character count
-    characterCount.textContent = text.length;
+    const characters = trimmedText.length;
 
     // Words count
-    const words = text.split(/\s+/).filter((word) => word.length > 0);
-
-    wordCount.textContent = words.length;
+    const words = trimmedText.split(/\s+/).filter((word) => word.length > 0 && word !== '...');
 
     // Sentence count
-    const sentences = text.split(/[.!?]+/).filter((sen) => sen.length > 0);
-    
-    sentenceCount.textContent = sentences.length;
+    const sentences = trimmedText
+      .split(/[.!?]+/)
+      .filter((sentence) => sentence.trim().length > 0 && sentence.trim() !== '...');
+
+    return { characters, words: words.length, sentences: sentences.length };
+  };
+
+  input.addEventListener("input", () => {
+    const { characters, words, sentences } = calculateCounts(input.value);
+
+    characterCount.textContent = characters;
+    wordCount.textContent = words;
+    sentenceCount.textContent = sentences;
   });
 });
